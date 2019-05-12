ed2ea79a5ad0d9e17525880f1e421075154c8ad5
BeCarefulPillar <465836845@qq.com>
2019-05-12 13:24:16 +0800

    add

---
+++ game/Assets/Script/GameData/GameData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public partial class GameData {
+    private static readonly GameData mInstance = new GameData();
+
+    public Dictionary<int, HeroData> heros;
+
+    public GameData() {
+        heros = new Dictionary<int, HeroData>();
+    }
+
+    public GameData Instance {
+        get {
+            return mInstance;
+        }
+    }
+}
+
+
---
+++ game/Assets/Script/Hero/HeroData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class HeroData {
+    private int mHp;
+    private int mMaxHp;
+    private int mAttack;
+    private int mDefend;
+
+    public int id;
+    public int heroId;
+
+    public HeroData(int id, int heroId) {
+        this.id = id;
+        this.heroId = heroId;
+    }
+}
