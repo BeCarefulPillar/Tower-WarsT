56c76c47b1ae442fa342e946c2501bdccb821cf0
sunkang <465836845@qq.com>
2019-05-14 15:43:20 +0800

    up

---
+++ game/Assets/Script/UI/ViewLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewLogin : MonoBehaviour {
+    public Button pressButton;
+    public InputField input;
+
+    void Start() {
+        pressButton.onClick.AddListener(Login);
+    }
+
+    void Update() {
+
+    }
+
+    void Login() {
+        Debug.Log("1111111111");
+        pressButton.gameObject.SetActive(false);
+        input.gameObject.SetActive(true);
+    }
+}
