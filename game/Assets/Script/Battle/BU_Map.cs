9c1f18880359ad4838a21c94d1ab8cfe42d85b75
sunkang <465836845@qq.com>
2019-05-20 16:34:24 +0800

    Revert "Revert "xw battle""
    This reverts commit c65d54ffbbbd892d01c7bdfbdc76b8ef13dcfe92.

---
+++ game/Assets/Script/Battle/BU_Map.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+//数据+显示
+
+public class BU_Map : MonoBehaviour
+{
+    private List<BU_Unit> mUnits = new List<BU_Unit>();
+}
---
+++ game/Assets/Script/Battle/Data/BD_Unit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class BD_Unit
+{
+    private BD_Map mMap;
+    private static int _gid = 1;
+    private int mID;
+    private int mBirthFrame;
+    private int mDeathFrame;
+    private Vector3 mPos;
+    private BU_Unit mBody;
+
+    public BD_Unit(BD_Map map)
+    {
+        if (map == null)
+            throw new Exception("map is null");
+        mMap = map;
+        mID = _gid++;
+        //mBirthFrame = 0;
+        //mDeathFrame = 0;
+        //mMap.Add(this);
+    }
+
+    public virtual void Update()
+    {
+    }
+
+    public BU_Unit body { get { return mBody; } set { if (value != mBody) mBody = value; } }
+}
