295ddd3b06e18f2d9b2c940fe77a272b8fd54c2a
小心柱子 <465836845@qq.com>
2020-05-08 22:15:57 +0800

    up delete all

--- game/Assets/Script/Battle/BU_Map.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-//数据+显示
-
-public class BU_Map : MonoBehaviour
-{
-    private List<BU_Unit> mUnits = new List<BU_Unit>();
-}
--- game/Assets/Script/Battle/Data/BD_Unit.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-public class BD_Unit
-{
-    public bool isMe = true;
-}
